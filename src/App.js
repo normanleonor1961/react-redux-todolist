1963da68f49cf035ced99099c9a2622f8d526a5c
normanleonor1961 <109060058+normanleonor1961@users.noreply.github.com>
2022-08-25 19:23:15 +0800

    components update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Todo List</h1>
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import TodoBody from "./TodoBody";
+import TodoHeader from "./TodoHeader";
+
+export default function TodoList() {
+  return (
+    <header>
+      <h1>Task List 2022</h1>
+      <TodoHeader />
+      <br />
+      <hr />
+      <br />
+      <br />
+      <TodoBody />
+    </header>
+  );
+}
